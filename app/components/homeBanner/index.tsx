a26dcf20baef442d9f4d6113f26ae765b6f49066
root <root@DESKTOP-SKBK36U>
2024-03-16 20:22:01 -0300

    fix

--- app/components/homeBanner/index.tsx
+++ app/components/homeBanner/index.tsx
@@ -6,16 +6,15 @@ import ButtonMain from "../buttonMain";
 
 export default function MainBanner() {
   return (
-    <section className={styles.main} style={{marginTop: "160px"}}>
+    <>
+    <div className={styles.heightDetail} style={{marginTop: "160px"}}>
+
+    </div>
+    <section className={styles.main}>
         <div className={styles.mainContainer}>
-            <h1>Há mais de <strong>35 anos</strong><br />
-            <span>concretizando sonhos</span></h1>
-            <ButtonMain textstring="Quero realizar meu sonho!"  />
-            <a href="/">
-              <p className={styles.TelBanner}><strong>Telefone </strong><span>&nbsp;(11) 4994-0522</span></p>
-            </a>
+              
         </div>
-        <Image className={styles.mainImageResponsive} width={1200} height={700} alt="Familia feliz ao adquirir os empreendimentos Vila Sul" src="/home_15650568 (1).jpg"></Image>
     </section>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -66,12 +66,13 @@ export default function Home() {
       <Navbar />
       <main className={styles.main}>
         
-        
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        <div className={styles.quemSomosImage} style={{backgroundImage: 'url("/responsivoForms.png")' }} ></div>
+        <MainBanner />
+        <div className={styles.detailsDiv}>
+       
+        </div>
+        <div className={styles.quemSomosImage} style={{backgroundImage: 'url("/responsivoForms.png")', marginBottom: '0px' }} ></div>
           
           <div className={styles.gradientHere}>
-            <br /><br /><br /><br />
             <iframe className={styles.mainIframe} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117440.17846930564!2d-46.81427562007078!3d-23.096891679726948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ceda1245a3cd57%3A0x506aea31895b681d!2sJarinu%20-%20Aglomera%C3%A7%C3%A3o%20Urbana%20de%20Jundia%C3%AD%2C%20Jarinu%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1710377068035!5m2!1spt-BR!2sbr" style={{border:0}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             {
               content?.fotos && <Fotos data={content?.fotos} />
