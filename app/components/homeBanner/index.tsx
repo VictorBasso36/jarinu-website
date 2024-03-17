3d1101874a802c7927fdac566c3e852942050ac3
root <root@DESKTOP-SKBK36U>
2024-03-17 00:39:28 -0300

    fix banner

--- app/components/homeBanner/index.tsx
+++ app/components/homeBanner/index.tsx
@@ -8,11 +8,20 @@ export default function MainBanner() {
   return (
     <>
     <div className={styles.heightDetail} style={{marginTop: "160px"}}>
-
+        <Image width={1500} height={70} src="/mainbgbannerTop.png" alt=""></Image>
     </div>
     <section className={styles.main}>
+        <Image className={styles.bannerimg} width={1500} height={630} src="/mainbgbannerBottom.png" alt=""></Image>
         <div className={styles.mainContainer}>
-              
+            <div className={styles.contentH1}>
+              <h1><span>Alto Padrão de</span><br />
+              <strong>JARINU</strong></h1>
+              <div className={styles.detail}></div>
+              <p>More no interior de São Paulo com muita qualidade de vida, segurança e elegância.</p>
+              <div className={styles.mainIcons}>
+
+              </div>
+            </div>
         </div>
     </section>
     </>
